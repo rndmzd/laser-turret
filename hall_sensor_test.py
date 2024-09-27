7512b26d5f7ecd26a0229abf2ccfae9fb26d19b6
rndmzd <rndmzd@protonmail.com>
2024-09-27 17:52:18 -0500

    feat(hall_sensor_test.py): add support for an additional Hall sensor pin fix(hall_sensor_test.py): rename existing pin variable for clarity and differentiate between multiple sensors

--- hall_sensor_test.py
+++ hall_sensor_test.py
@@ -5,20 +5,22 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin connected to the Hall sensor
-HALL_SENSOR_PIN = 22  # You can change this if using a different pin
+HALL_SENSOR_X = 27  # You can change this if using
+HALL_SENSOR_Y = 22  # You can change this if using a different pin
 
 # Set up the GPIO pin as input (the external pull-up resistor is used)
-GPIO.setup(HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(HALL_SENSOR_X, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(HALL_SENSOR_Y, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Hall Sensor Test Program")
 print("Press Ctrl+C to exit")
 
 try:
     while True:
-        if GPIO.input(HALL_SENSOR_PIN) == GPIO.LOW:
-            print("Magnet detected!")
-        else:
-            print("No magnet.")
+        if GPIO.input(HALL_SENSOR_Y) == GPIO.LOW:
+            print("Y magnet detected!")
+        if GPIO.input(HALL_SENSOR_X) == GPIO.LOW:
+            print("X magnet detected!")
         time.sleep(0.5)
 except KeyboardInterrupt:
     print("Program terminated by user")
