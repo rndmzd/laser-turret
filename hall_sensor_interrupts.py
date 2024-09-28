537044c9975ac694de7705e7cbd78d84c6c4d042
rndmzd <rndmzd@protonmail.com>
2024-09-27 19:25:48 -0500

    chore(hall_sensor_interrupts.py): comment out Sensor 1 code to disable its functionality for testing purposes of Sensor 2 only

--- hall_sensor_interrupts.py
+++ hall_sensor_interrupts.py
@@ -2,15 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 # Define GPIO pin numbers for the sensors
-SENSOR_1_PIN = 17  # GPIO pin for Sensor 1
+# SENSOR_1_PIN = 17  # GPIO pin for Sensor 1
 SENSOR_2_PIN = 27  # GPIO pin for Sensor 2
 
 # Callback functions to handle events
-def sensor1_callback(channel):
+"""def sensor1_callback(channel):
     if GPIO.input(SENSOR_1_PIN):
         print("Sensor 1 detected no magnetic field (HIGH)")
     else:
-        print("Sensor 1 detected a magnetic field (LOW)")
+        print("Sensor 1 detected a magnetic field (LOW)")"""
 
 def sensor2_callback(channel):
     if GPIO.input(SENSOR_2_PIN):
@@ -20,11 +20,11 @@ def sensor2_callback(channel):
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
-GPIO.setup(SENSOR_1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Enable internal pull-up for sensor 1
+# GPIO.setup(SENSOR_1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Enable internal pull-up for sensor 1
 GPIO.setup(SENSOR_2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Enable internal pull-up for sensor 2
 
 # Event detection on both rising and falling edges
-GPIO.add_event_detect(SENSOR_1_PIN, GPIO.BOTH, callback=sensor1_callback)
+# GPIO.add_event_detect(SENSOR_1_PIN, GPIO.BOTH, callback=sensor1_callback)
 GPIO.add_event_detect(SENSOR_2_PIN, GPIO.BOTH, callback=sensor2_callback)
 
 try:
